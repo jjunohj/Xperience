0a6320d2c457f5282cd0c252a0f6ba7d8e8d9c8c
jjunohj <jjunohj@gmail.com>
2024-03-22 23:53:00 +0900

    feat: 포스팅 목록 레이아웃 구현

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
-import { Header } from "src/components/Header";
 import "../styles/globals.css";
+import Layout from "../components/layouts/Layout";
+import { Toaster } from "react-hot-toast";
+import Providers from "../components/layouts/Providers";
 
 export default function RootLayout({
   children,
@@ -9,12 +11,22 @@ export default function RootLayout({
   return (
     <html>
       <head>
-        <title>Contentlayer Next.js Example</title>
+        <title>jjunohj</title>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
       </head>
       <body>
-        <Header />
-        <div className="px-6">{children}</div>
+        <Providers>
+          <div className="px-6 dark:bg-neutral-900 dark:text-neutral-100">
+            <Layout>{children}</Layout>
+
+            <Toaster
+              toastOptions={{
+                className: "text-primary bg-secondary",
+                position: "top-center",
+              }}
+            />
+          </div>
+        </Providers>
       </body>
     </html>
   );

655072c40348222027ab6fe9a94aeab110c17c69
jjunohj <jjunohj@gmail.com>
2024-03-22 23:54:03 +0900

    feat: 포스트 상세 페이지 구현

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from "date-fns";
 import { allPosts } from "contentlayer/generated";
 import { getMDXComponent } from "next-contentlayer/hooks";
+import PostLayout from "@/src/components/layouts/PostLayout";
 
 export const generateStaticParams = async () =>
   allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
@@ -10,22 +11,12 @@ export const generateMetadata = ({ params }) => {
   return { title: post.title };
 };
 
-const PostLayout = ({ params }: { params: { slug: string } }) => {
+const Page = ({ params }: { params: { slug: string } }) => {
   const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
 
   const Content = getMDXComponent(post.body.code);
 
-  return (
-    <article className="py-8 mx-auto max-w-xl">
-      <div className="mb-8 text-center">
-        <time dateTime={post.date} className="mb-1 text-xs text-gray-600">
-          {format(parseISO(post.date), "LLLL d, yyyy")}
-        </time>
-        <h1>{post.title}</h1>
-      </div>
-      <Content />
-    </article>
-  );
+  return <PostLayout post={post} />;
 };
 
-export default PostLayout;
+export default Page;

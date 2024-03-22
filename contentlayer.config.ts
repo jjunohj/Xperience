dbb46eaf444487459a7064fcebd465700fd400e5
jjunohj <jjunohj@gmail.com>
2024-03-22 23:50:36 +0900

    config: 포스트 속성 추가

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,30 +1,49 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, makeSource } from "contentlayer/source-files";
 
 const Post = defineDocumentType(() => ({
-  name: 'Post',
+  name: "Post",
   filePathPattern: `**/*.mdx`,
-  contentType: 'mdx',
+  contentType: "mdx",
   fields: {
     title: {
-      type: 'string',
-      description: 'The title of the post',
+      type: "string",
+      description: "The title of the post",
       required: true,
     },
+    description: {
+      type: "string",
+      required: true,
+    },
+    thumbnail: {
+      type: "string",
+      required: false,
+    },
     date: {
-      type: 'date',
-      description: 'The date of the post',
+      type: "date",
+      description: "The date of the post",
+      required: true,
+    },
+    category: {
+      type: "string",
+      description: "The category of the post",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+      description: "The tags of the post",
       required: true,
     },
   },
   computedFields: {
     url: {
-      type: 'string',
+      type: "string",
       resolve: (doc) => `/posts/${doc._raw.flattenedPath}`,
     },
   },
-}))
+}));
 
 export default makeSource({
-  contentDirPath: 'posts',
+  contentDirPath: "posts",
   documentTypes: [Post],
-})
+});

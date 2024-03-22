30453ef879c10fdff2c69c65f4ac52cf1cb6fb99
jjunohj <jjunohj@gmail.com>
2024-03-22 23:49:44 +0900

    feat: 메뉴 설정

---
+++ config.ts
@@ -0,0 +1,37 @@
+export const headerConfig = {};
+
+export const siteConfig = {
+  title: "jjunohj",
+  description: "개발하면서 탐구한 것을 소소하게 기록하는 공간입니다.",
+  copyright: "jjunohj © All rights reserved.",
+  since: 2024,
+  googleAnalyticsId: "",
+  author: {
+    name: "Jun Cheong",
+    photo: "",
+    bio: "Junior Frontend Engineer",
+    contacts: {
+      email: "jjunohj@gmail.com",
+      github: "jjunohj",
+      twitter: "",
+      velog: "",
+      linkedin: "",
+      youtube: "",
+      instagram: "",
+    },
+  },
+  menus: [
+    {
+      label: "About",
+      path: "/about",
+    },
+    {
+      label: "Archives",
+      path: "/archives",
+    },
+    {
+      label: "Category",
+      path: "/category",
+    },
+  ],
+};

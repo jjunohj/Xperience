754ff8958176bc148017219bded40d5c04d4b82b
jjunohj <jjunohj@gmail.com>
2024-03-23 17:22:41 +0900

    refactor: 레이아웃 통합

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
-import Layout from "../components/layouts/Layout";
 import { Toaster } from "react-hot-toast";
 import Providers from "../components/layouts/Providers";
+import HeaderNav from "../components/layouts/HeaderNav";
 
 export default function RootLayout({
   children,
@@ -17,8 +17,11 @@ export default function RootLayout({
       <body>
         <Providers>
           <div className="px-6 dark:bg-neutral-900 dark:text-neutral-100">
-            <Layout>{children}</Layout>
-
+            <div className="mx-auto max-w-3xl px-6 lg:max-w-6xl lg:px-8">
+              <HeaderNav />
+              <main className="relative pb-16">{children}</main>
+              <footer className="pb-8 text-sm text-neutral-800 dark:text-neutral-400"></footer>
+            </div>
             <Toaster
               toastOptions={{
                 className: "text-primary bg-secondary",

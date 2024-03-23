f613d204bc71d7f72699bca9b119ebcdaf3f5ff5
jjunohj <jjunohj@gmail.com>
2024-03-23 17:23:48 +0900

    feat: 네비바 메뉴 아이콘 공통 컴포넌트 추가

---
+++ src/components/common/NavItem.tsx
@@ -0,0 +1,28 @@
+import { usePathname } from "next/navigation";
+
+import { $ } from "@/src/libs/core";
+
+import LinkHover from "./LinkHover";
+
+export default function NavItem({
+  href,
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"a">) {
+  const pathname = usePathname();
+  const isActive = pathname === href;
+
+  return (
+    <LinkHover
+      {...props}
+      href={href}
+      className={$(
+        isActive ? "text-primary font-semibold" : "text-secondary font-normal",
+        className
+      )}
+    >
+      {children}
+    </LinkHover>
+  );
+}

828427aa186977a054dbb73d83355a7fd29d00ec
jjunohj <jjunohj@gmail.com>
2024-03-23 17:22:59 +0900

    feat: 가로선 공통 컴포넌트 추가

---
+++ src/components/common/Hr.tsx
@@ -0,0 +1,16 @@
+import { $ } from "@/src/libs/core";
+
+export default function Hr({
+  className,
+  ...props
+}: React.ComponentProps<"hr">) {
+  return (
+    <hr
+      {...props}
+      className={$(
+        "border-1 w-full border-neutral-300 transition-all dark:border-neutral-700",
+        className
+      )}
+    />
+  );
+}

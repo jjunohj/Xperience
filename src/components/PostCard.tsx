6dfe05e30797b756ddcbad15b71bf57b7b36bc50
jjunohj <jjunohj@gmail.com>
2024-06-08 16:29:24 +0900

    feat: PostCard 컴포넌트 구현

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,29 +1,50 @@
 import { Post } from "contentlayer/generated";
-import { getMDXComponent } from "next-contentlayer/hooks";
 import { format, parseISO } from "date-fns";
-import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import CalenderIcon from "./icons/CalenderIcon";
+import { fadeInUp } from "../constants/animations";
 
 const PostCard = (post: Post) => {
-  const Content = getMDXComponent(post.body.code);
-
   return (
-    <div className="mb-8 ">
-      <h2 className="text-xl ">
-        <Link
-          href={post.url}
-          className="text-blue-700 hover:text-blue-900"
-          legacyBehavior
-        >
-          {post.title}
-        </Link>
-      </h2>
-      <time dateTime={post.date} className="block mb-2 text-xs text-gray-600">
-        {format(parseISO(post.date), "LLLL d, yyyy")}
-      </time>
-      <div className="text-sm">
-        <Content />
+    <motion.a
+      href={post.url}
+      className="overflow-hidden rounded-xl bg-neutral-200 dark:bg-neutral-800"
+      variants={fadeInUp}
+      whileHover={{ scale: 1.05, transformOrigin: "center" }}
+      whileTap={{ scale: 0.95 }}
+    >
+      <div className="w-56 h-72 rounded-2xl relative overflow-hidden">
+        <Image
+          src={post.thumbnail}
+          alt={post.title}
+          width={224}
+          height={288}
+          draggable={false}
+          className="object-cover w-full h-full"
+        />
+        <div className="absolute inset-0 dark:bg-black dark:bg-opacity-20" />
+
+        <div className="absolute h-[136px] gap-2 flex flex-col justify-start items-start w-full bottom-0 left-0 p-4 bg-opacity-60 bg-white dark:bg-black dark:bg-opacity-30">
+          <div className="flex items-center gap-2">
+            <CalenderIcon className="w-4 -translate-y-[1.5px]" />
+            <span className="text-xs text-black dark:text-white">
+              {format(parseISO(post.date), "yyyy.MM.dd")}
+            </span>
+          </div>
+
+          <h2 className="text-md text-black font-semibold dark:text-white">
+            {post.title}
+          </h2>
+        </div>
+
+        <div className="absolute bottom-4 left-4">
+          <span className="bg-primary text-black font-semibold dark:text-white text-xs py-1 px-2 rounded">
+            {post.category}
+          </span>
+        </div>
       </div>
-    </div>
+    </motion.a>
   );
 };
 

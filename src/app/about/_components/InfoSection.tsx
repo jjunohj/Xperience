906dc2f010baa30ffda1a019c4eac3e566c52a99
jjunohj <jjunohj@gmail.com>
2024-06-20 01:37:46 +0900

    feat: 전체 레이아웃 스타일링 세부 조정

--- src/app/about/_components/InfoSection.tsx
+++ src/app/about/_components/InfoSection.tsx
@@ -5,7 +5,7 @@ type InfoSectionProps = {
 
 const InfoSection = ({ title, children }: InfoSectionProps) => {
   return (
-    <section className="w-full flex h-fit mt-8 border-t-[1px] mx-8 pt-8">
+    <section className="w-full flex h-fit mt-8 border-t-[1px] pt-8">
       <h1 className="w-1/4 text-2xl font-semibold text-orange-400 dark:text-white pl-8">
         {title}
       </h1>

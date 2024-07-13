2d2d777392ec0bd817b2036236b6606dfbd2a224
jjunohj <jjunohj@gmail.com>
2024-07-13 14:11:34 +0900

    Feat : 이력서 페이지 반응형 구현

--- src/app/about/_components/InfoSection.tsx
+++ src/app/about/_components/InfoSection.tsx
@@ -6,7 +6,7 @@ type InfoSectionProps = {
 const InfoSection = ({ title, children }: InfoSectionProps) => {
   return (
     <section className="w-full flex h-fit mt-8 border-t-[1px] pt-8">
-      <h1 className="w-1/4 text-2xl font-semibold text-orange-400 dark:text-white pl-8">
+      <h1 className="w-1/3 sm:w-1/4 lg:w-1/4 text-lg sm:text-xl lg:text-2xl font-semibold text-point pl-4 sm:pl-6 lg:pl-8">
         {title}
       </h1>
       <div className="w-full flex flex-col gap-6">{children}</div>

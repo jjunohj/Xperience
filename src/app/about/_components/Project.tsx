3196d750f031d9d72e3c8388f01db5193f225294
jjunohj <jjunohj@gmail.com>
2024-07-13 14:47:25 +0900

    Design : 반응형 구현

--- src/app/about/_components/Project.tsx
+++ src/app/about/_components/Project.tsx
@@ -3,8 +3,8 @@ import { Project } from "./type";
 
 const Project = (props: Project) => {
   return (
-    <div className="w-full flex flex-col gap-0.5">
-      <div className="w-full flex flex-col-reverse sm:flex-row justify-between items-start lg:pr-8">
+    <div className="w-full flex flex-col gap-1.5">
+      <div className="w-full flex flex-col-reverse sm:flex-row justify-between items-start">
         <h1 className="text-base sm:text-lg lg:text-xl font-semibold dark:text-white">
           {props.title}
         </h1>
@@ -18,7 +18,7 @@ const Project = (props: Project) => {
       <span className="text-black text-sm sm:text-base dark:text-white">
         {props.role}
       </span>
-      <div className="flex w-4/5 flex-wrap gap-1 my-1 text-sm sm:text-base text-gray-500 dark:text-white font-light">
+      <div className="flex w-4/5 flex-wrap gap-x-1 gap-y-0.5 my-1 text-sm sm:text-base text-gray-500 dark:text-white font-light">
         {props.stacks.map((stack, index) => (
           <React.Fragment key={index}>
             {index !== 0 && <span>|</span>}
@@ -26,7 +26,7 @@ const Project = (props: Project) => {
           </React.Fragment>
         ))}
       </div>
-      <ul className="flex text-sm sm:text-base flex-col gap-1">
+      <ul className="flex text-sm sm:text-base flex-col gap-0.5">
         {props.info?.map((info, index) => (
           <li
             className="text-black font-extralight dark:text-white"

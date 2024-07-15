b8ac63032a075cd6f5debde856ec6933a8ae9b4b
jjunohj <jjunohj@gmail.com>
2024-07-15 18:45:00 +0900

    Chore : 바뀐 Post 속성에 따라 코드 수정

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -9,31 +9,31 @@ import IconText from "./common/IconText";
 const PostCard = (post: Post) => {
   return (
     <motion.a
-      href={post.url}
+      href={post.slug}
       className="w-full overflow-hidden rounded-xl shadow-sm drop-shadow-xl dark:bg-neutral-800 sm:bg-neutral-200"
       variants={fadeInUp}
       whileHover={{ scale: 1.05, transformOrigin: "center" }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="relative h-24 w-full rounded-2xl sm:h-72 sm:w-56">
+      <div className="relative h-24 w-full rounded-2xl sm:h-60 sm:w-40 md:h-72 md:w-56">
         <Image
           src={post.thumbnail}
           alt={post.title}
           width={224}
           height={288}
           draggable={false}
-          className="h-24 w-1/3 object-cover  dark:brightness-75 dark:contrast-125 dark:grayscale sm:h-72 sm:w-56"
+          className="h-24 w-1/4 object-cover  dark:brightness-75 dark:contrast-125 dark:grayscale sm:h-60 sm:w-40 md:h-72 md:w-56"
         />
-        <div className="absolute inset-0 w-1/3 bg-gradient-to-r from-transparent to-white opacity-50 dark:to-black sm:hidden" />
+        <div className="absolute inset-0 w-1/4 bg-gradient-to-r from-transparent to-white opacity-50 dark:to-black sm:hidden" />
         <div className="absolute inset-0 dark:bg-black dark:bg-opacity-20" />
 
-        <div className="absolute bottom-0 right-0 flex h-full w-2/3 flex-col items-start justify-start gap-1.5 bg-white bg-opacity-60 p-4 dark:bg-black dark:bg-opacity-30 sm:left-0 sm:h-[136px] sm:w-full sm:gap-2">
+        <div className="absolute bottom-0 right-0 flex h-full w-3/4 flex-col items-start justify-start gap-1.5 overflow-hidden bg-white bg-opacity-60 p-4 dark:bg-black dark:bg-opacity-30 sm:left-0 sm:h-[136px] sm:w-full sm:gap-2">
           <IconText
             Icon={CalenderIcon}
             text={format(parseISO(post.date), "yyyy.MM.dd")}
           />
 
-          <h2 className="text-sm font-semibold text-black dark:text-white sm:text-base">
+          <h2 className="flex-wrap break-words text-sm font-semibold text-black dark:text-white sm:text-base">
             {post.title}
           </h2>
         </div>

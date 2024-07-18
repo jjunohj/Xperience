66e0cf1d432064c0ca4cd38dc43c9c0d622e47e7
jjunohj <jjunohj@gmail.com>
2024-07-18 22:32:20 +0900

    Fix : 게시글에 태그가 많을 경우 잘리는 이슈 해결

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -28,13 +28,13 @@ const PostCard = (post: Post) => {
         <div className="absolute inset-0 w-1/4 bg-gradient-to-r from-transparent to-white opacity-50 dark:to-black sm:hidden" />
         <div className="absolute inset-0 dark:bg-black dark:bg-opacity-20" />
 
-        <div className="absolute bottom-0 right-0 flex h-full w-3/4 flex-col items-start justify-start gap-1 overflow-hidden bg-white bg-opacity-60 px-4 py-2 dark:bg-black dark:bg-opacity-30 sm:left-0 sm:h-[136px] sm:w-full sm:gap-2 sm:p-4">
+        <div className="absolute bottom-0 right-0 flex h-full w-3/4 flex-col items-start justify-start gap-1 overflow-hidden bg-white bg-opacity-60 px-4 py-2 dark:bg-black dark:bg-opacity-30 sm:left-0 sm:h-[156px] sm:w-full sm:gap-2 sm:p-4">
           <IconText
             Icon={CalenderIcon}
             text={format(parseISO(post.date), "yyyy.MM.dd")}
           />
 
-          <h2 className="flex-wrap overflow-x-hidden text-ellipsis whitespace-nowrap text-sm font-semibold text-black dark:text-white sm:break-words sm:text-base">
+          <h2 className="flex-wrap overflow-x-hidden text-ellipsis whitespace-nowrap text-sm font-semibold text-black dark:text-white sm:overflow-x-auto sm:text-clip sm:whitespace-normal sm:break-words sm:text-base">
             [{post.category}] {post.title}
           </h2>
           <div className="flex gap-2 text-xs opacity-90 sm:hidden">
@@ -46,12 +46,12 @@ const PostCard = (post: Post) => {
           </div>
         </div>
 
-        <div className="absolute bottom-4 left-4 hidden sm:block">
-          <div className="flex gap-2 text-sm opacity-90">
+        <div className="absolute bottom-4 left-4 hidden w-full sm:block">
+          <div className="flex flex-wrap gap-2 text-sm opacity-90">
             {post.tags.map((tag) => (
               <Pill
                 key={tag}
-                className="bg-neutral-50 font-light dark:bg-neutral-900"
+                className="bg-neutral-50 px-1.5 py-0.5 text-xs font-light dark:bg-neutral-900"
               >
                 {tag}
               </Pill>

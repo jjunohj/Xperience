d937d21147fb4987e52f151f5237faaea6ad58a1
jjunohj <jjunohj@gmail.com>
2024-08-01 00:47:57 +0900

    Feat : 게시글 카드 디자인 변경

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -11,41 +11,33 @@ const PostCard = (post: Post) => {
   return (
     <motion.a
       href={post.slug}
-      className="w-full overflow-hidden shadow-sm dark:bg-neutral-800 sm:bg-neutral-200 sm:drop-shadow-lg"
+      className="group h-56 w-full overflow-hidden p-4 sm:h-96"
       variants={fadeInSlideToRight}
-      whileHover={{ scale: 1.01, transformOrigin: "center" }}
+      whileHover={{ scale: 0.98 }}
       whileTap={{ scale: 0.95 }}
     >
-      <div className="relative h-96 w-[44rem]">
+      <div className="relative h-full w-full">
         <Image
           src={post.thumbnail}
           alt={post.title}
           width={224}
           height={288}
           draggable={false}
-          className="h-full w-full object-cover dark:brightness-90 dark:grayscale"
+          className="h-full w-full object-cover group-hover:drop-shadow-base-bold dark:brightness-90"
         />
-        <div className="absolute inset-0 w-1/4 bg-gradient-to-r from-transparent to-white opacity-50 dark:to-black sm:hidden" />
-        <div className="absolute inset-0 dark:bg-black dark:bg-opacity-20" />
 
-        <div className="absolute bottom-0 right-0 flex h-full w-3/4 flex-col items-start justify-start gap-1 overflow-hidden bg-white bg-opacity-60 px-4 py-2 dark:bg-black dark:bg-opacity-30 sm:left-0 sm:h-[156px] sm:w-full sm:gap-2 sm:p-4">
-          <h2 className="flex-wrap overflow-x-hidden text-ellipsis whitespace-nowrap text-sm font-semibold sm:overflow-x-auto sm:text-clip sm:whitespace-normal sm:break-words sm:text-2xl">
-            [{post.category}] {post.title}
+        <div className="absolute bottom-0 left-0 right-0 flex h-[116px] w-full flex-col items-start justify-start gap-2 overflow-hidden bg-white bg-opacity-60 p-3 dark:bg-black dark:bg-opacity-30 sm:h-[128px] sm:p-4">
+          <h2 className="flex-wrap overflow-x-hidden text-clip whitespace-normal break-words text-sm font-semibold group-hover:drop-shadow-base-bold sm:overflow-x-auto sm:text-xl">
+            {post.title}
           </h2>
           <IconText
+            className="gap-1 text-xs font-light group-hover:drop-shadow-base-bold sm:text-sm"
             Icon={CalenderIcon}
-            text={format(parseISO(post.date), "yyyy.MM.dd")}
+            text={format(parseISO(post.date), "yyyy년 MM월 dd일")}
           />
-          <div className="flex gap-2 text-xs opacity-90 sm:hidden">
-            {post.tags.map((tag) => (
-              <Pill key={tag} className="bg-secondary font-light">
-                {tag}
-              </Pill>
-            ))}
-          </div>
         </div>
 
-        <div className="absolute bottom-4 left-4 hidden w-full sm:block">
+        <div className="absolute bottom-3 left-3 block w-full sm:bottom-4 sm:left-4">
           <div className="flex flex-wrap gap-2 text-sm opacity-90">
             {post.tags.map((tag) => (
               <Pill
--- src/components/PostCards.tsx
+++ src/components/PostCards.tsx
@@ -11,9 +11,9 @@ const PostCards = () => {
       variants={staggerHalf}
       initial="initial"
       animate="animate"
-      className="mt-16 flex w-full flex-col items-center gap-3 sm:w-fit"
+      className="mt-8 flex w-full flex-col items-center gap-3 sm:mt-16 sm:w-fit"
     >
-      <h2 className="mb-4 text-2xl font-extrabold tracking-tight">
+      <h2 className="text-base font-semibold sm:mb-4 sm:text-2xl sm:tracking-tight">
         RECENT POSTS
       </h2>
       <motion.div variants={staggerHalf} className="flex w-full flex-col gap-4">

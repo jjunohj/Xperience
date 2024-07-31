5b532d63510c1c98a4f3390dac9b6986c60944ad
jjunohj <jjunohj@gmail.com>
2024-08-01 00:49:26 +0900

    Chore : 블로그 홈 페이지 요소 간 간격 조정

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -18,7 +18,7 @@ export default function BlogPage() {
   );
 
   return (
-    <div className="mx-auto flex w-full max-w-6xl flex-col gap-2">
+    <div className="mx-auto flex w-full max-w-6xl flex-col gap-2 p-4">
       <h1 className="text-4xl font-extrabold lg:text-5xl">Blog</h1>
       <span className="text-sm font-light text-gray-600 dark:text-gray-400">
         다시 보고 싶은 기술들을 저만의 언어로 공유합니다.
@@ -28,12 +28,18 @@ export default function BlogPage() {
         placeholder="카테고리, 게시글 검색"
         onChange={searchHandler}
       />
-      <h3 className="mt-8 text-2xl font-extrabold sm:text-3xl">Categories</h3>
+      <h3 className="mt-4 text-2xl font-extrabold sm:mt-8 sm:text-3xl">
+        Categories
+      </h3>
       <Categories categories={filteredCategories} />
-      <h2 className="mt-8 text-2xl font-extrabold sm:text-3xl">Posts</h2>
-      {filteredBlogPosts.map((post) => (
-        <PostListItem key={post.slug} post={post} />
-      ))}
+      <h2 className="mt-4 text-2xl font-extrabold sm:mt-8 sm:text-3xl">
+        Posts
+      </h2>
+      <div className="mt-2 flex flex-col gap-2 sm:grid sm:grid-cols-2 sm:gap-12">
+        {filteredBlogPosts.map((post) => (
+          <PostListItem key={post.slug} post={post} />
+        ))}
+      </div>
     </div>
   );
 }

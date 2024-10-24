1c9e6359cf083ca84f0ff0634001729ef7e8c4ca
jjunohj <jjunohj@gmail.com>
2024-10-24 21:49:05 +0900

    design: 폰트 변경

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,7 +35,6 @@ module.exports = {
         sans: ["var(--font-sans)", ...fontFamily.sans],
         spoqa: ["var(--font-spoqa)", ...fontFamily.sans],
         mono: ["var(--font-mono)", ...fontFamily.mono],
-        arizonia: ["var(--font-arizonia)"],
       },
       typography: (theme) => ({
         DEFAULT: {

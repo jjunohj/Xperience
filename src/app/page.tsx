0e8f6c148d797602caf18b12a2d9b8476daf7937
jjunohj <jjunohj@gmail.com>
2024-10-24 21:51:06 +0900

    feat: 모바일 뷰 자기소개 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Pill from "../components/common/Pill";
 export default function HomePage() {
   return (
     <div className="flex h-full flex-col items-center">
-      <section className="relative mb-4 flex h-72 w-full items-center justify-center gap-4 p-4 sm:mb-8 sm:h-[32rem] sm:gap-6 lg:gap-10">
+      <section className="relative mb-4 hidden h-72 w-full items-center justify-center gap-4 p-4 sm:mb-8 sm:flex sm:h-[32rem] sm:gap-6 lg:gap-10">
         <Image
           src="/images/profile.png"
           alt="Profile"
@@ -27,17 +27,20 @@ export default function HomePage() {
           <div className="flex w-1/2 flex-col justify-center sm:w-fit">
             <h3 className="text-sm font-normal sm:mb-1 sm:text-xl lg:mb-2">
               사용자 중심 개발자
+              <span className="ml-2 text-sm font-extralight text-neutral-400">
+                @xuuno
+              </span>
             </h3>
             <h1 className="mb-2 text-3xl font-extrabold sm:mb-4 sm:text-6xl">
               CHEONG JUNHO
             </h1>
             <p className="hidden font-extralight text-gray-600 dark:text-gray-400 sm:block">
-              문제를 발견하고 해결하는 것에 재미를 느끼며, 불가능한 것은 없다고
-              생각합니다.
+              사용자가 아주 상식적이고 쾌적한 경험을 갖는 그 작고 당연한 가치를
+              최우선으로 여깁니다.
             </p>
             <p className="hidden font-extralight text-gray-600 dark:text-gray-400 sm:mb-2 sm:block lg:mb-6">
-              과정이 아무리 험난해도, 유저가 상식적이고 편리한 경험을 갖도록
-              하는 것이 더욱 중요하다고 생각합니다.
+              비효율적인 개발 환경과 프로세스를 파악하고 개선하며, 개발의
+              생산성에 대해 늘 고민합니다.
             </p>
             <Link href="/about" className="flex w-40 justify-start">
               <Pill className="rounded-full px-3 py-2 text-xs font-light text-neutral-600 dark:text-neutral-400 sm:px-4 sm:py-2 sm:text-sm">
@@ -47,6 +50,7 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+      <div className="h-10 w-full sm:hidden" />
       <PostCards />
     </div>
   );

26384987d887ec36ecf9f051961a4552fa38418f
jjunohj <jjunohj@gmail.com>
2024-12-14 21:36:36 +0900

    chore: 구글 서치 콘솔 색인 설정

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,8 +4,8 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
-      disallow: "/about",
+      allow: ["/"],
+      disallow: "/api",
     },
     sitemap: "https://blog.xuuno.me/sitemap.xml",
   };

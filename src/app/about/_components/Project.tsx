5d131f0d32db94df5d5a9bea0f2a6b03c839dfef
jjunohj <jjunohj@gmail.com>
2024-12-18 22:36:59 +0900

    style: Project 아이템 컴포넌트 Prettier 적용

--- src/app/about/_components/Project.tsx
+++ src/app/about/_components/Project.tsx
@@ -3,22 +3,22 @@ import { Project } from "./type";
 
 const Project = (props: Project) => {
   return (
-    <div className="w-full flex flex-col gap-1.5">
-      <div className="w-full flex flex-col-reverse sm:flex-row justify-between items-start">
-        <h1 className="text-base sm:text-lg lg:text-xl font-semibold dark:text-white">
+    <div className="flex w-full flex-col gap-1.5">
+      <div className="flex w-full flex-col-reverse items-start justify-between sm:flex-row">
+        <h1 className="text-base font-semibold dark:text-white sm:text-lg lg:text-xl">
           {props.title}
         </h1>
-        <span className="text-base sm:text-lg lg:text-xl w-fit font-light sm:font-semibold dark:text-white">
+        <span className="w-fit text-base font-light dark:text-white sm:text-lg sm:font-normal lg:text-xl">
           {props.period}
         </span>
       </div>
-      <span className="text-black text-sm sm:text-base dark:text-white">
+      <span className="text-sm text-black dark:text-white sm:text-base">
         {props.description}
       </span>
-      <span className="text-black text-sm sm:text-base dark:text-white">
+      <span className="text-sm text-black dark:text-white sm:text-base">
         {props.role}
       </span>
-      <div className="flex w-4/5 flex-wrap gap-x-1 gap-y-0.5 my-1 text-sm sm:text-base text-gray-500 dark:text-white font-light">
+      <div className="my-1 flex w-4/5 flex-wrap gap-x-1 gap-y-0.5 text-sm font-light text-gray-500 dark:text-white sm:text-base">
         {props.stacks.map((stack, index) => (
           <React.Fragment key={index}>
             {index !== 0 && <span>|</span>}
@@ -26,10 +26,10 @@ const Project = (props: Project) => {
           </React.Fragment>
         ))}
       </div>
-      <ul className="flex text-sm sm:text-base flex-col gap-0.5">
+      <ul className="flex flex-col gap-0.5 text-sm sm:text-base">
         {props.info?.map((info, index) => (
           <li
-            className="text-black font-extralight dark:text-white"
+            className="font-extralight text-black dark:text-white"
             key={index}
           >
             · {info}

24dab2fb5c54e4f3a352f669cd190bc8be539a65
jjunohj <jjunohj@gmail.com>
2025-06-06 23:58:13 +0900

    refactor: Providers 컴포넌트 간소화 및 기본 테마 설정 추가

--- src/components/layouts/Providers.tsx
+++ src/components/layouts/Providers.tsx
@@ -1,23 +1,11 @@
-"use client";
-
 import { ThemeProvider } from "next-themes";
-import { useEffect, useState } from "react";
-import dayjs from "dayjs";
 
-const Providers = ({ children }) => {
-  const [isMount, setIsMount] = useState(false);
-
-  useEffect(() => {
-    setIsMount(true);
-  }, []);
-
-  if (!isMount) {
-    return null;
-  }
-
-  dayjs.locale("ko");
-
-  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      {children}
+    </ThemeProvider>
+  );
 };
 
 export default Providers;

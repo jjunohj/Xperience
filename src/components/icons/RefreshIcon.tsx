c541c8b19482e0bbb8c7acf82ab3aab3cfa19ce1
jjunohj <121740394+jjunohj@users.noreply.github.com>
2025-09-09 21:49:02 +0900

    feat: 새로고침 아이콘 추가

---
+++ src/components/icons/RefreshIcon.tsx
@@ -0,0 +1,25 @@
+export default function RefreshIcon({
+  className,
+  ...props
+}: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      height="16"
+      width="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className={className}
+      {...props}
+    >
+      <path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8" />
+      <path d="M21 3v5h-5" />
+      <path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16" />
+      <path d="M8 16H3v5" />
+    </svg>
+  );
+}

ce93beae54f5619a04f65e107f08122eb1752429
jjunohj <121740394+jjunohj@users.noreply.github.com>
2025-09-30 10:22:36 +0900

    fix: 파일 처리 방식 attachment에서 inline으로 변경

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ["image/avif", "image/webp"],
     dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
+    contentDispositionType: "inline",
     remotePatterns: [
       {
         protocol: "https",
